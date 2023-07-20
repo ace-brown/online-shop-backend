89b9e6e63f0df01f5e3ad2d2a59cdda654c3287d
ace-brown <kazem_abdi@yahoo.com>
2023-07-20 17:44:48 +0200

    add admin controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,21 +1,36 @@
+const { render } = require("ejs");
 const Product = require("../models/product");
+
+exports.getIndex = () => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
 
 exports.getProduct = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/product-list", {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
+      pageTitle: "All Products",
+      path: "/products",
     });
   });
 };
 
 exports.cart = (req, res, next) => {
   res.render("shop/cart", {
-    pageTitle: "Cart",
-    path: "/shop/cart",
+    pageTitle: "Your Cart",
+    path: "/cart",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    pageTitle: "Checkout",
+    path: "/checkout",
   });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -11,4 +11,5 @@ router.get("/add-product", adminController.getAddProduct);
 
 router.post("/add-product", adminController.postAddProduct);
 
+router.get("/products", adminController.getProducts);
 module.exports = router;
