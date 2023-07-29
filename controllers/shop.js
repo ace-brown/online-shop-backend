3799a96afa344f7c8c0e058e61354f9a863a650f
ace-brown <kazem_abdi@yahoo.com>
2023-07-29 19:08:54 +0200

    add: product model using sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,34 +3,46 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getIndex = (req, res) => {
-  Product.fetchAll((products) => {
-    res.render("shop/index", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/",
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/index", {
+        prods: rows,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 };
 
 exports.getProducts = (req, res) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/product-list", {
+        prods: rows,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 };
 
 exports.getProduct = (req, res) => {
-  const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: "Product Detail",
-      path: "/products",
+  const productId = req.params.productId;
+  Product.findById(productId)
+    .then(([product]) => {
+      res.render("shop/product-detail", {
+        product: product[0],
+        pageTitle: "Product Detail",
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 };
 
 exports.getCart = (req, res) => {
--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,9 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-// Creating a pool of connections instead of a single connection, that allow us
-// to reach out to pool whenever we have a query to run and onece the query is done
-// wil be handed back into the pool, and pool can be finished when app is shut down
-const pool = mysql.createPool({
+// Setup db connection pool thru sequelize
+const sequelize = new Sequelize("node-complete", "root", "mysqlpassword", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "mysqlpassword",
 });
 
-// Use promise to help us handle asyn tasks
-module.exports = pool.promise();
+module.exports = sequelize;
